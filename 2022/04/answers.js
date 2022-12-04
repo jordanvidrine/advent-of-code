b82e559ca2ed5f8b88bc9fbaa8eb264fd0f1a452
Jordan Vidrine <jordan@jordanvidrine.com>
2022-12-04 08:24:04 -0600

    2022 day 04

---
+++ 2022/04/answers.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+let answer1 = fs
+  .readFileSync("./input.txt", { encoding: "utf8" })
+  .split(/\r?\n/)
+  .reduce((previousValue, item) => {
+    let assignments = item.split(",");
+    let elf1 = /(^\d*)-(\d*$)/g.exec(assignments[0].trim());
+    let elf2 = /(^\d*)-(\d*$)/g.exec(assignments[1]);
+    let a, b, c, d;
+
+    a = Number(elf1[1]);
+    b = Number(elf1[2]);
+    c = Number(elf2[1]);
+    d = Number(elf2[2]);
+
+    if ((a >= c && b <= d) || (c >= a && d <= b)) {
+      return previousValue + 1;
+    }
+    return previousValue;
+  }, 0);
+
+console.log("one",answer1);
+
+let answer2 = fs
+  .readFileSync("./input.txt", { encoding: "utf8" })
+  .split(/\r?\n/)
+  .reduce((previousValue, item) => {
+    let assignments = item.split(",");
+    let elf1 = /(^\d*)-(\d*$)/g.exec(assignments[0].trim());
+    let elf2 = /(^\d*)-(\d*$)/g.exec(assignments[1]);
+    let a, b, c, d;
+
+    a = Number(elf1[1]);
+    b = Number(elf1[2]);
+    c = Number(elf2[1]);
+    d = Number(elf2[2]);
+
+    if (
+      (b >= c && b <= d) ||
+      (a >= c && a <= d) ||
+      (a >= c && b <= d) ||
+      (c >= a && d <= b)
+    ) {
+      return previousValue + 1;
+    }
+    return previousValue;
+  }, 0);
+
+console.log("two",answer2);
